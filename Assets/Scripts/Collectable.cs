6ee8b738fde67aaa3bf2f2339cc4388049af88eb
Caleb Wolthers <w0473807@nscc.ca>
2024-04-15 18:36:56 -0300

    f

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -1,29 +1,22 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
 
-
     public static event Action OnCollected;
-
     public static int total;
-
 
     void Awake()
     {
         total++;
     }
 
-
-
     void Update()
     {
         transform.Rotate(0, 0.5f, 0.5f);
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
